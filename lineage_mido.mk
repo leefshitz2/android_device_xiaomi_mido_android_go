769e0b9b0b5f0ea005a1bb9f207f5cd9d64799fc
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    mido: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from mido device
+AB_OTA_UPDATER := false
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common LineageOS stuff.
